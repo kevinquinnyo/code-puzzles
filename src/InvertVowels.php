6eb56b4bd51819e5242a86e6aae0be032c5ef81a
Kevin Quinn <kevin.quinn@totalserversolutions.com>
2019-02-10 19:27:57 -0500

    initial

---
+++ src/InvertVowels.php
@@ -0,0 +1,66 @@
+<?php
+namespace kevinquinnyo\CC;
+
+class InvertVowels
+{
+    private $string;
+    const VOWELS = [
+        'a', 'e', 'i', 'o', 'u',
+        'A', 'E', 'I', 'O', 'U',
+    ];
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    public function getPowerSet(array $array): array
+    {
+        $results= [[]]; // include empty set
+        foreach ($array as $value) {
+            foreach ($results as $set) {
+                $results[] = array_merge(array($value), $set);
+            }
+        }
+
+        return $results;
+    }
+
+    public function getVowelPairs(): array
+    {
+        $chars = str_split($this->string);
+        $found = 0;
+        foreach ($chars as $idx => $char) {
+            if (in_array($char, self::VOWELS)) {
+                ++$found;
+                if ($found === 2) {
+                    $matches[$idx - 1] = [$chars[$idx - 1], $char];
+                    $found = 0;
+                }
+            }
+        }
+
+        return $matches;
+    }
+
+    public function getPermutations(array $options= []): array
+    {
+        $options += ['includeOriginalString' => true];
+        $mutations = [];
+        $pairs = $this->getVowelPairs();
+        $powerSet = $this->getPowerSet(array_keys($pairs));
+        foreach ($powerSet as $keys) {
+            $mutated = $this->string;
+            foreach ($keys as $key) {
+                $mutated[$key] = $pairs[$key][1];
+                $mutated[$key + 1]= $pairs[$key][0];
+            }
+            $mutations[] = $mutated;
+        }
+
+        if ($options['includeOriginalString'] === false) {
+            unset($mutations[array_search($this->string)]);
+        }
+
+        return $mutations;
+    }
+}
---
+++ tests/InvertVowelsTest.php
@@ -0,0 +1,88 @@
+<?php
+namespace kevinquinnyo\CC\Test;
+
+use kevinquinnyo\CC\InvertVowels;
+use PHPUnit\Framework\TestCase;
+
+class InvertVowelsTest extends TestCase
+{
+    public function testGetPowerSet()
+    {
+        $set = [
+            1,
+            2,
+            3
+        ];
+
+        $expected = [
+            [],
+            [1],
+            [2],
+            [3],
+            [2, 1],
+            [3, 1],
+            [3, 2],
+            [3, 2, 1],
+        ];
+
+        $InvertVowels = new InvertVowels('asdf');
+        $powerSet = $InvertVowels->getPowerSet($set);
+        sort($powerSet);
+
+        $this->assertEquals($expected, $powerSet);
+    }
+
+    public function testGetVowelPairs()
+    {
+        $string = 'cruel boat';
+        $InvertVowels = new InvertVowels($string);
+        $expected = [
+            2 => ['u', 'e'],
+            7 => ['o', 'a'],
+        ];
+
+        $pairs = $InvertVowels->getVowelPairs();
+        $this->assertEquals($expected, $pairs);
+    }
+
+    public function testGetPermutations()
+    {
+        $string = 'ROAD BOAT PHOENIX';
+        $InvertVowels = new InvertVowels($string);
+
+        $expected = [
+            'ROAD BOAT PHOENIX',
+            'RAOD BOAT PHOENIX',
+            'ROAD BAOT PHOENIX',
+            'RAOD BAOT PHOENIX',
+            'ROAD BOAT PHEONIX',
+            'RAOD BOAT PHEONIX',
+            'ROAD BAOT PHEONIX',
+            'RAOD BAOT PHEONIX',
+        ];
+        $permutations = $InvertVowels->getPermutations();
+
+        $this->assertEquals($expected, $permutations);
+    }
+
+    public function testGetPermutationsWithoutOriginalString()
+    {
+        $string = 'ROAD BOAT PHOENIX';
+        $InvertVowels = new InvertVowels($string);
+
+        $expected = [
+            'RAOD BOAT PHOENIX',
+            'ROAD BAOT PHOENIX',
+            'RAOD BAOT PHOENIX',
+            'ROAD BOAT PHEONIX',
+            'RAOD BOAT PHEONIX',
+            'ROAD BAOT PHEONIX',
+            'RAOD BAOT PHEONIX',
+        ];
+
+        $options['includeOriginalString'] = false;
+        $permutations = $InvertVowels->getPermutations($options);
+
+        $this->assertEquals($expected, $permutations);
+    }
+}
